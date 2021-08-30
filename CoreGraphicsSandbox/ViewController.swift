cd76c8fe633e51c92318a9c6f9a5592217e9d993
AtinAgnihotri <atinagnihotri@gmail.com>
2021-08-30 08:11:19 +0530

    Sets up the sandbox

--- CoreGraphicsSandbox/ViewController.swift
+++ CoreGraphicsSandbox/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var drawImageView: UIImageView!
+    var currentDrawType = 0 {
+        didSet { // Wrapping value from 5
+            if currentDrawType > 5 {
+                currentDrawType = 0
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        drawRectangle()
+    }
+    
+    func drawRectangle() {
+        
     }
 
-
+    @IBAction func redrawTapped(_ sender: UIButton) {
+        currentDrawType += 1
+        
+        switch currentDrawType {
+        case 0:
+            drawRectangle()
+        default:
+            break
+        }
+    }
+    
 }
 
